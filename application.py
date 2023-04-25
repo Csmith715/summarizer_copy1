c6402349ad133d951fa02872cf4e4036e88a31bc
Cormac <csmith715@gmail.com>
2023-04-25 17:58:10 -0400

    Revert "question generation fix"
    This reverts commit 3a935310bf697b0f798a60985be99d5cff2d1efd.

--- application.py
+++ application.py
@@ -107,7 +107,10 @@ def generatequestions():
     inputtext = req_data.get('text', '')
     email_cta_text = req_data.get('email_cta_prompt', '')
     logger.info('Generating Questions')
-    data['generated question'] = question_model.predict(inputtext)
+    if question_model:
+        data['generated question'] = question_model.predict(inputtext)
+    else:
+        data['generated question'] = []
     data['email_ctas'] = email_cta_creations(email_cta_text)
     logger.info('Questions Created')
 
