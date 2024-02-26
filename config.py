7a4e36f6d013e4a1cd8d023bf2df5d15bcf1c64f
Csmith715 <35317679+Csmith715@users.noreply.github.com>
2024-02-26 16:21:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import re
 
 # OPEN API STUFF
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'tbd'
 
 url_string = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{" \
              r"};:'\".,<>?«»“”‘’])) "
